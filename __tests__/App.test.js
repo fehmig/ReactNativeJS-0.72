6fee0ff636a4622b2642419e2096a293c02ee491
fehmiig <gunay.fehmi14@gmail.com>
2023-07-04 16:35:33 +0300

    first commit

---
+++ __tests__/App.test.js
@@ -0,0 +1,11 @@
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+// Note: import explicitly to use the types shiped with jest.
+import { it } from '@jest/globals';
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  renderer.create(<App />);
+});
